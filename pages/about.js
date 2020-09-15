88699301c3ec8e0b9e8ed87fb06d11d1ed0be366
ayva-source <raj@ayvashopping.com>
2020-09-15 23:36:26 +0700

    Start A Next js Project

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const About = () => (
+
+    <Layout>
+        <div> 
+            <h1>About Page for this  App</h1>
+        </div>
+    </Layout>
+);
+
+export default About;
